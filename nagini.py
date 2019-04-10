632337cf8b51a83116cc4c5feb10ee6f1ad15b27
Parth Krishna <thenerdsuperuser@gmail.com>
2019-04-10 19:06:30 +0530

    :snake:

--- nagini.py
+++ nagini.py
@@ -28,7 +28,7 @@ def exec_user_input(i, user_input, user_globals):
                 user_input, user_globals
             )
     except Exception as e:
-        print('%s : %s' % (e.__class__.__name__))
+        print('%s : %s' % (e.__class__.__name__, e))
 
     else:
         if retval is not None:
@@ -36,14 +36,32 @@ def exec_user_input(i, user_input, user_globals):
 
     return user_globals
 
+
+def selected_user_globals(user_globals):
+    return (
+        (key, user_globals[key])
+        for key in sorted(user_globals)
+        if not key.startswith('__') or not key.endswith('__')
+        )
+
+
+def save_user_globals(user_globals, path='user_globals.txt'):
+    with open(path, 'w') as fd:
+        for key, val in selected_user_globals(user_globals):
+            fd.write('%s = %s(%s)\n' % (
+                key, val, val.__class__.__name__
+                ))
+
 def main():
 
     user_globals = {}
+    save_user_globals(user_globals)
 
     for i, user_input in get_user_input():
         user_globals = exec_user_input(
                 i, user_input, user_globals
             )
+        save_user_globals(user_globals)
 
 if __name__ == '__main__':
     main()
