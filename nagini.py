e3c8dd5281da9c8721124609a7f1dc0500544150
Parth Krishna <thenerdsuperuser@gmail.com>
2019-04-10 18:43:29 +0530

    catches exceptions

--- nagini.py
+++ nagini.py
@@ -11,8 +11,26 @@ def get_user_input():
             break
 
 
+def exec_function(user_input):
+
+    try:
+        compile(user_input, '<stdin>', 'eval')
+    except SyntaxError:
+        return exec
+    return eval
+
 def exec_user_input(i, user_input, user_globals):
-    exec(user_input, user_globals)
+
+    try:
+        retval = exec_function(user_input)(
+                user_input, user_globals
+            )
+    except Exception as e:
+        print('%s : %s' % (e.__class__.__name__))
+
+    else:
+        if retval is not None:
+            print('Out [%d]: %s' % (i, retval))
 
 def main():
 
